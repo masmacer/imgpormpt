7ccd57f0d58e475e9d9633f00452eee7e35b7387
masmacer <1453919814@qq.com>
2025-09-11 14:18:03 +0800

    xxx

--- packages/common/src/test-import.ts
+++
@@ -1,5 +0,0 @@
-import { CozeApiService, cozeApiService, FileUploadResponse, WorkflowRunResponse } from './coze-api';
-
-console.log('CozeApiService:', typeof CozeApiService);
-console.log('cozeApiService:', typeof cozeApiService);
-console.log('Success!');
