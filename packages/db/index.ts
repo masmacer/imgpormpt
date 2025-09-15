c4435e20993857fef1781ce0f3b616ef5b6b870d
masmacer <1453919814@qq.com>
2025-09-15 17:17:51 +0800

    xxx

--- packages/db/index.ts
+++ packages/db/index.ts
@@ -7,4 +7,7 @@ export { jsonArrayFrom, jsonObjectFrom } from "kysely/helpers/postgres";
 export * from "./prisma/types";
 export * from "./prisma/enums";
 
-export const db = createKysely<DB>();
+// 明确指定连接字符串，并根据环境选择合适的连接方式
+export const db = createKysely<DB>({
+  connectionString: process.env.POSTGRES_URL_NON_POOLING || process.env.DATABASE_URL || process.env.POSTGRES_URL
+});
